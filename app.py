85e50a78f828aa8081766e55916583c05d008c50
mikespikeai <mike@m24u.nl>
2025-07-23 17:05:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,29 +4,27 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from llama_index.core import VectorStoreIndex, ServiceContext, SimpleDirectoryReader
-from llama_index.llms import OpenAI
+from llama_index.core.llms import OpenAI
 
 from sitemap_loader import SitemapReader
 
-# Laad documenten uit je sitemap
+# Laad data uit sitemap
 SITEMAP_URL = "https://eiwitlimonade.nl/sitemap_index.xml"
 documents = SitemapReader().load_data(SITEMAP_URL)
 
-# Instellingen voor OpenAI + indexering
-llm = OpenAI(model="gpt-3.5-turbo")
+# AI setup
+llm = OpenAI(model="gpt-3.5-turbo", api_key=os.getenv("OPENAI_API_KEY"))
 service_context = ServiceContext.from_defaults(llm=llm)
 index = VectorStoreIndex.from_documents(documents, service_context=service_context)
 query_engine = index.as_query_engine()
 
-# Start FastAPI
+# App setup
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-
 
 @app.get("/", response_class=HTMLResponse)
 async def form_get(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
 
 @app.post("/query", response_class=HTMLResponse)
 async def query(request: Request, vraag: str = Form(...)):
